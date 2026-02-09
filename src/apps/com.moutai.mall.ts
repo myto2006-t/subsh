0b94035f6dde5c23b1a3a155b0acc3822443a4c7
马玉涛 <mayutao@bonc.com.cn>
2026-02-09 17:56:20 +0800

    init

---
+++ src/apps/com.moutai.mall.ts
@@ -0,0 +1,58 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.moutai.mall',
+  name: 'i茅台',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-首页弹窗广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: [
+            'com.moutai.mall.MainActivity',
+            'com.moutai.mall.module.splash.SplashActivity',
+          ],
+          matches:
+            '[id="com.moutai.mall:id/ivPic"] + ViewGroup + [id="com.moutai.mall:id/vClose"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/12745130',
+            'https://i.gkd.li/i/12745153',
+          ],
+        },
+      ],
+    },
+    {
+      key: 10,
+      name: '权限提示-通知权限',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          activityIds: '.MainActivity',
+          matches:
+            '@[id="com.moutai.mall:id/vClose"] - LinearLayout > [text*="开通消息通知"]',
+          snapshotUrls: 'https://i.gkd.li/i/12745142',
+        },
+      ],
+    },
+    {
+      key: 11,
+      name: '马年经典',
+      matchTime: 3600000,
+      actionMaximum: 1,
+      resetMatch: 'match',
+      rules: [
+        {
+          activityIds: ['com.moutai.mall.webview.view.WebViewActivity'],
+          matches: 'TextView[text="自选生产日期(快递配送)"]',
+          snapshotUrls: ['https://i.gkd.li/i/25179790'],
+        },
+      ],
+    },
+  ],
+});
